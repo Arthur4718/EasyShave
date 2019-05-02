c8a01bb1a247c822ece23949ed8960ede17dca9d
arthur <arthur.gomes@sportsmatch.com.br>
2019-05-02 14:56:44 -0300

    Login e-mail activity with veryfication of valid e-mail and password.

--- app/src/main/java/com/supermatch/smplay/activity/RegisterActivities/LoginEmailActivity.kt
+++ app/src/main/java/com/supermatch/smplay/activity/RegisterActivities/LoginEmailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.supermatch.smplay.R
 import com.supermatch.smplay.activity.BaseActivities.BaseActivity
+import com.supermatch.smplay.utils.ValidationsUtils
 import kotlinx.android.synthetic.main.activity_login_email.*
 
 class LoginEmailActivity : BaseActivity() {
@@ -14,17 +15,39 @@ class LoginEmailActivity : BaseActivity() {
         setContentView(R.layout.activity_login_email)
 
 
+        initActions()
 
-        tvBackNavigation.setOnClickListener {
-            Toast.makeText(context, "Up Navigation", Toast.LENGTH_SHORT).show()
-        }
+
+    }
+
+    private fun initActions() {
+
+        tvBackNavigation.setOnClickListener { finish() }
+
+        btnLoginWithEmail.setOnClickListener {  logWithUserData() }
+
 
         tvRecoverAccount.setOnClickListener {
             Toast.makeText(context, "Recover Account", Toast.LENGTH_SHORT).show()
         }
 
         tvCreateAccount.setOnClickListener {
-            Toast.makeText(context, "Recover Account", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Create Account", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    private fun logWithUserData() {
+        if (ValidationsUtils.verifyLoginAccount(edtLoginEmail, edtLoginPassword)) {
+
+            val userLogin = edtLoginEmail.text.toString()
+            val userPassword = edtLoginPassword.text.toString()
+
+            Toast.makeText(context, "Verifying Account...", Toast.LENGTH_SHORT).show()
+
         }
     }
+
+
 }
