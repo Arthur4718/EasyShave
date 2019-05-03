c4fbf3c08b82c830e1c129f851c4948375649cc7
arthur <arthur.gomes@sportsmatch.com.br>
2019-05-03 14:00:47 -0300

    Complete profile activity

---
+++ app/src/main/java/com/supermatch/smplay/activity/RegisterActivities/CompleteProfile.kt
@@ -0,0 +1,13 @@
+package com.supermatch.smplay.activity.RegisterActivities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.supermatch.smplay.R
+
+class CompleteProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complete_profile)
+    }
+}
