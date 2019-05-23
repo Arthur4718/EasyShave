b42af1238ca03bd8c585f4460ce2796a74aee86b
arthur <arthur.gomes.4718@gmail.com>
2019-05-23 19:20:35 -0300

    Data Item Adapter

---
+++ app/src/main/java/com/devarthur/easyshave/adapter/DataItemAdapter.kt
@@ -0,0 +1,58 @@
+package com.devarthur.easyshave.adapter
+
+
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import android.widget.TextView
+import com.devarthur.easyshave.AgendaDetalhe
+import org.jetbrains.anko.startActivity
+import com.devarthur.easyshave.R
+import com.devarthur.easyshave.dataModel.DataItem
+import com.devarthur.easyshave.dataModel.Servico
+import java.util.ArrayList
+
+class DataItemAdapter(val dataList : ArrayList<DataItem>) : RecyclerView.Adapter<DataItemAdapter.ViewHolder>()  {
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): DataItemAdapter.ViewHolder {
+
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.dataServico_item, viewGroup, false)
+        return ViewHolder(v)
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userCard : DataItem = dataList[position]
+
+        holder.txtData.text = userCard.mData
+
+
+        holder.card.setOnClickListener {
+
+
+
+
+        }
+
+    }
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        //capturar as views do card
+        val txtData = itemView.findViewById(R.id.txtDataDetalheHorario) as TextView
+        val card = itemView.findViewById(R.id.cardContent) as CardView
+
+
+    }
+
+
+}
