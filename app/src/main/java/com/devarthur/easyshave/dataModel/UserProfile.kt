b27791a4a51e9d20672a593d1c656c6975e3b1c9
Arthur <arthur.gomes_4718@hotmail.com>
2019-05-26 17:09:57 -0300

    Add data dialog

---
+++ app/src/main/java/com/devarthur/easyshave/dataModel/UserProfile.kt
@@ -0,0 +1,6 @@
+package com.devarthur.easyshave.dataModel
+
+//* Created by Arthur Gomes at 2019-05-23 22:10 - contact me at devarthur4718@gmail.com.br
+
+
+data class UserProfile(val uid: String, val username : String, val useremail : String, val userbirthdate : String, val userType : Int)
