de4bf93c30cb6540a6dbaee5e6ecc5af391efb82
Arthur <arthur.gomes_4718@hotmail.com>
2019-05-28 23:21:09 -0300

    Estabelecimento Data model

---
+++ app/src/main/java/com/devarthur/easyshave/dataModel/EstabelecimentoModel.kt
@@ -0,0 +1,5 @@
+package com.devarthur.easyshave.dataModel
+
+//* Created by Arthur Gomes at 2019-05-28 23:19 - contact me at devarthur4718@gmail.com.br
+
+data class EstabelecimentoModel (var nomeEstabelecimento : String, var localEstabelecimento : String, var distancia : String)
